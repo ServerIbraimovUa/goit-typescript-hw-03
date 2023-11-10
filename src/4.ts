6682b90a2889b1e7f3cd25ecb95245641df1a83e
ServerIbraimovUa <serveribraimov7@gmail.com>
2023-11-10 10:08:18 +0200

    hw-03

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,59 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {
+    this.signature = signature;
+  }
 
-const house = new MyHouse(key);
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+abstract class House {
+  door: boolean;
+  key: Key;
+  tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+    this.door = false;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Open");
+    } else {
+      console.log("closed");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door === true;
+      console.log("open");
+    } else {
+      console.log("closed");
+    }
+  }
+}
+
+const key = new Key(Math.floor(Math.random() * 20));
+console.log(key);
 const person = new Person(key);
+const house = new MyHouse(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
-
-export {};
